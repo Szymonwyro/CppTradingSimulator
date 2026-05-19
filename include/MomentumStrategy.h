46870930a9f060bfb3def73b0d7a37fded478c36
Szymon Wyrozumski <szymwyro@icloud.com>
2026-05-19 10:12:59 +0100

    Change inputs for the MomentumStrategy for tunable params: lookbackperiod rather than just using t-1, threshold to improve confidence in momentum, and track priceHistory to allow for lookbackperiod tuning

--- include/MomentumStrategy.h
+++ include/MomentumStrategy.h
@@ -4,12 +4,21 @@
 #include "Order.h"
 #include "PriceStep.h"
 
+#include <deque>
+
 class MomentumStrategy : public Strategy {
     private:
         double momentumFactor;
-    
+        int lookbackPeriod;
+        double threshold;
+        std::deque<double> priceHistory;
+
     public:
-    MomentumStrategy(double momentumFactor);
+        MomentumStrategy(
+            double momentumFactor,
+            int lookbackPeriod,
+            double threshold
+        );
 
     Order generateOrder(
         int traderId,
