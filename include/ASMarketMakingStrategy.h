29656108897072a06ff73195f0f0ba66c43dc727
Szymon Wyrozumski <szymwyro@icloud.com>
2026-05-24 23:19:27 +0100

    Build out inputs for AS Market Maker

---
+++ include/ASMarketMakingStrategy.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Strategy.h"
+#include "Order.h"
+#include "PriceStep.h"
+
+class ASMarketMakingStrategy : public Strategy {
+    private:
+        double gamma; // risk aversion parameter
+        double orderSize; // fixed order size
+        int horizon; // time horizon for the strategy
+        double k; // market making parameter
+        double inventory;
+
+    public:
+        ASMarketMakingStrategy(
+            double gamma,
+            double orderSize,
+            int horizon,
+            double k,
+            double initialInventory = 0.0
+        );
+
+    std::vector<Order> generateOrders(
+        int traderId,
+        const PriceStep& market
+    ) override;
+
+    double onFill(double qty); // Update inventory and return PnL from the fill
+};
