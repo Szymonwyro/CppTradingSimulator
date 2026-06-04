4604700e6e959fdd2047d1ff97225c1132845027
Szymon Wyrozumski <szymwyro@icloud.com>
2026-06-04 22:46:55 +0100

    fix order of arguments for generateOrders, hardcode volatility for now

--- src/strategies/ASMarketMakingStrategy.cpp
+++ src/strategies/ASMarketMakingStrategy.cpp
@@ -15,13 +15,14 @@ ASMarketMakingStrategy::ASMarketMakingStrategy(
 //}
 
 std::vector<Order> ASMarketMakingStrategy::generateOrders(
+    int traderId,
     const std::vector<PriceStep>& history
 ) {
     const PriceStep& market = history.back();
     double t = market.day / static_cast<double>(horizon); // Normalize time to [0,1]
     double timeRemaining = (1.0 - t);
 
-    double sigma = market.vol;
+    double sigma = 0.2; // market.vol; added later
     double reservationPrice = market.mid - inventory * gamma * sigma * sigma * timeRemaining;
     double halfSpread = 1.0 / gamma * std::log(1 + gamma / k) + 0.5 * gamma * sigma * sigma * timeRemaining;
 
