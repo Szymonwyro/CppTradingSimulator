6de2e5648b08cecb52aae6e3d50d2bbfbe132b12
Szymon Wyrozumski <szymwyro@icloud.com>
2026-06-07 22:15:32 +0100

    Match type with .cpp

--- include/NewsGenerator.h
+++ include/NewsGenerator.h
@@ -1,9 +1,10 @@
 #pragma once
 #include <vector>
 #include "NewsEvent.h"
+#include "RandomEngine.h"
 
 class NewsGenerator {
     public:
-        std::vector<NewsEvent> generateNews(int horizon);
+        std::vector<NewsEvent> generateNews(int horizon, RandomEngine& rng);
 
 };
