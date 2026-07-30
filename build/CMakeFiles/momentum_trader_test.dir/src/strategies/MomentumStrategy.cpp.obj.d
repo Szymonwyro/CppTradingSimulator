30c6f71653010fabb90ab5928590b47dab6a42c8
Szymon Wyrozumski <szymwyro@icloud.com>
2026-07-30 22:58:15 +0100

    updates to CMAKE

--- build/CMakeFiles/momentum_trader_test.dir/src/strategies/MomentumStrategy.cpp.obj.d
+++ build/CMakeFiles/momentum_trader_test.dir/src/strategies/MomentumStrategy.cpp.obj.d
@@ -106,4 +106,20 @@ CMakeFiles/momentum_trader_test.dir/src/strategies/MomentumStrategy.cpp.obj: \
  C:/msys64/ucrt64/include/c++/15.2.0/bits/stl_vector.h \
  C:/msys64/ucrt64/include/c++/15.2.0/bits/stl_bvector.h \
  C:/msys64/ucrt64/include/c++/15.2.0/bits/vector.tcc \
- C:/Users/szymw/TradingSimulator/CppEngine/include/NewsEvent.h
+ C:/Users/szymw/TradingSimulator/CppEngine/include/NewsEvent.h \
+ C:/msys64/ucrt64/include/c++/15.2.0/cmath \
+ C:/msys64/ucrt64/include/math.h \
+ C:/msys64/ucrt64/include/c++/15.2.0/bits/specfun.h \
+ C:/msys64/ucrt64/include/c++/15.2.0/limits \
+ C:/msys64/ucrt64/include/c++/15.2.0/tr1/gamma.tcc \
+ C:/msys64/ucrt64/include/c++/15.2.0/tr1/special_function_util.h \
+ C:/msys64/ucrt64/include/c++/15.2.0/tr1/bessel_function.tcc \
+ C:/msys64/ucrt64/include/c++/15.2.0/tr1/beta_function.tcc \
+ C:/msys64/ucrt64/include/c++/15.2.0/tr1/ell_integral.tcc \
+ C:/msys64/ucrt64/include/c++/15.2.0/tr1/exp_integral.tcc \
+ C:/msys64/ucrt64/include/c++/15.2.0/tr1/hypergeometric.tcc \
+ C:/msys64/ucrt64/include/c++/15.2.0/tr1/legendre_function.tcc \
+ C:/msys64/ucrt64/include/c++/15.2.0/tr1/modified_bessel_func.tcc \
+ C:/msys64/ucrt64/include/c++/15.2.0/tr1/poly_hermite.tcc \
+ C:/msys64/ucrt64/include/c++/15.2.0/tr1/poly_laguerre.tcc \
+ C:/msys64/ucrt64/include/c++/15.2.0/tr1/riemann_zeta.tcc
